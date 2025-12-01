fc883a3326eb68a90cdcd4bb1f83bf0187fa208f
RK-01 <91008997+RK-01@users.noreply.github.com>
2025-12-01 07:52:29 +0530

    heuristics completed

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -48,24 +48,24 @@ const BookingForm = ({ availableTimes, dispatch, submitForm }) => {
             <form className="w-full" onSubmit={formik.handleSubmit}>
 				<div className="row form-group">
 			        <div className="form-control">
-					    <label className="form-label"  htmlFor="first_name">First Name</label>
-					<input name="firstName" type="text" id="first_name" className="form-input" {...formik.getFieldProps("firstName")} />
+					    <label className="form-label"  htmlFor="firstNameInput">First Name</label>
+					<input name="firstName" type="text" id="firstNameInput" className="form-input" {...formik.getFieldProps("firstName")} />
 					 {formik.touched.firstName && formik.errors.firstName && (
           			<div style={{ color: "red" }}>{formik.errors.firstName}</div>)}
 					</div>
 				</div>
 				<div className="row form-group">
 					<div className="form-control">
-						<label className="form-label"  htmlFor="last_name">Last Name</label>
-						<input name="lastName" type="text" id="last_name" className="form-input" {...formik.getFieldProps("lastName")}/>
+						<label className="form-label"  htmlFor="lastNameInput">Last Name</label>
+						<input name="lastName" type="text" id="lastNameInput" className="form-input" {...formik.getFieldProps("lastName")}/>
 						 {formik.touched.lastName && formik.errors.lastName && (
           			<div style={{ color: "red" }}>{formik.errors.lastName}</div>)}
 				</div>
 				</div>
 				<div className="row form-group">
 					<div className="form-control">
-				    	<label className="form-label" htmlFor="email">Email</label>
-					<input name="email" type="email" id="email" className="form-input" {...formik.getFieldProps("email")} />
+				    	<label className="form-label" htmlFor="emailInput">Email</label>
+					<input name="email" type="email" id="emailInput" className="form-input" {...formik.getFieldProps("email")} />
 					{formik.touched.email && formik.errors.email && (
           			<div data-testid="emailerror" style={{ color: "red" }}>{formik.errors.email}</div>)}
 					</div>
@@ -73,16 +73,16 @@ const BookingForm = ({ availableTimes, dispatch, submitForm }) => {
                 <div className="form-grid">
                     <div className="row form-group">
 					    <div className="form-control">
-						    <label className="form-label" htmlFor="mobile">Mobile</label>
-						<input name="mobile" type="text" id="mobile" className="form-input" {...formik.getFieldProps("mobile")} />
+						    <label className="form-label" htmlFor="mobileInput">Mobile</label>
+						<input name="mobile" type="text" id="mobileInput" className="form-input" {...formik.getFieldProps("mobile")} />
 						{formik.touched.mobile && formik.errors.mobile && (
           			<div style={{ color: "red" }}>{formik.errors.mobile}</div>)}
 						</div>
                     </div>
                     <div className="row form-group">
 						<div className="form-control">
-							<label className="form-label" htmlFor="num_guests">Number of Guests</label>
-							<select name="guests" id="num_guests" className="form-input" {...formik.getFieldProps("guests")}>
+							<label className="form-label" htmlFor="numberOfGuests">Number of Guests</label>
+							<select name="guests" id="numberOfGuests" className="form-input" {...formik.getFieldProps("guests")}>
 					            <option value="2">Number of Guests</option>
 								<option value="1">1</option>
 								<option value="2">2</option>
@@ -98,16 +98,16 @@ const BookingForm = ({ availableTimes, dispatch, submitForm }) => {
                 <div className="form-grid">
                     <div className="row form-group">
 						<div className="form-control">
-							<label className="form-label" htmlFor="date">Date</label>
-						<input name="date" data-testid="date-change-event" type="date" id="date" className="form-input" {...formik.getFieldProps("date")} />
+							<label className="form-label" htmlFor="dateInput">Date</label>
+						<input name="date" data-testid="date-change-event" type="date" id="dateInput" className="form-input" {...formik.getFieldProps("date")} />
 							{formik.touched.date && formik.errors.date && (
           			<div style={{ color: "red" }}>{formik.errors.date}</div>)}
 					</div>
                     </div>
                     <div className="row form-group">
 						<div className="form-control">
-					        <label className="form-label" htmlFor="res-time">Choose Time</label>
-                            <select name="time" data-testid="select-times" id="res-time" {...formik.getFieldProps("time")} className="form-input">
+					        <label className="form-label" htmlFor="timeInput">Choose Time</label>
+                            <select name="time" data-testid="select-times" id="timeInput" {...formik.getFieldProps("time")} className="form-input">
 							<option data-testid="time-option" value="00:00">Select Time</option>
 							{availableTimes?.map((item, index) => {
 								return <option data-testid="time-option" className="available-time-container" key={index} >{item} </option>
@@ -132,7 +132,7 @@ const BookingForm = ({ availableTimes, dispatch, submitForm }) => {
                     </div>
 					<div className="row form-group">
 					<div className="form-control mt-10">
-					<button type="submit" className="reservation-confirm-btn" data-testid="confirm-booking" disabled={formik.isSubmitting}>Confirm Reservation</button>
+					<button aria-label="Click button to confirm booking a table" type="submit" className="reservation-confirm-btn" data-testid="confirm-booking" disabled={formik.isSubmitting}>Confirm Reservation</button>
 				</div>
 				</div>
             </form>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ const HeroSection = () => {
                 <span className="hero-subtitle">Chicago</span>
             </div>
             <p className="hero-body-text">Serving delightful recipes with a smile, everyday. Come and experience today!</p>
-           <a className="hero-action-btn" href="/reservation">Reserve A Table</a>
+           <a className="hero-action-btn" href="/reservation" aria-label="On Click">Reserve A Table</a>
             </div>
             <div className="hero-image-container">
                 <img className="hero-image" src="/restaurantfood.jpg" alt="Little Lemon" width={300}/>
